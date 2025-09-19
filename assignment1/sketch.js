784c133fc1aa56598a01726558923e72b042a80a
hk1280 <hk1280@nyu.edu>
2025-09-19 17:28:03 -0400

    updates to assignment 1

--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -4,9 +4,26 @@ function setup() {
 }
 
 function draw() {
+  fill("rgba(132, 189, 247, 1)");
+  noStroke();
+  rect(windowWidth*0.52,50,50,windowHeight);
+  // middle blue line down the middle
+  rect(windowWidth*0.32,windowHeight*0.45,windowWidth*0.68,50);
+  // blue straight line across the middle 
+
+  fill("rgb(0,0,0)");
+  rect(0,windowHeight*0.45+50,windowWidth*0.32,windowHeight*0.45+50)
+  // black box on the bottom left
+
   fill("rgba(112, 112, 112, 1)");
   noStroke();
   rect(0,0,windowWidth,50);
+  // gray line at the top
+  rect(0,windowHeight-50,windowWidth,50);
+  // gray line at the bottom
+
+
+
 
 
 }
