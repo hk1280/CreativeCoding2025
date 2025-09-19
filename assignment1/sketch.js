7f145c0c5ddc29339c676c322c1a6e1bbcda3264
hk1280 <hk1280@nyu.edu>
2025-09-19 17:43:03 -0400

    more to assignment 1

--- assignment1/sketch.js
+++ assignment1/sketch.js
@@ -1,5 +1,12 @@
+let myDiameter = 60;
+// setting the base diameter of the smaller circle
+// so I can use it as a variable
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  // making it scalable
+  background("rgba(255, 236, 236, 1)");
+  // cream colored background
 
 }
 
@@ -21,6 +28,15 @@ function draw() {
   // gray line at the top
   rect(0,windowHeight-50,windowWidth,50);
   // gray line at the bottom
+
+  noFill();
+  stroke("rgba(253, 0, 0, 1)");
+  strokeWeight(3);
+  circle(windowWidth*0.9,windowHeight*0.2,myDiameter);
+  // smaller red circle on the right
+
+  stroke("rgba(252, 255, 101, 1)");
+  circle(windowWidth*0.4,windowHeight*0.33,myDiameter*1.55);
 
 
 
