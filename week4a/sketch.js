124d54b4e25adf19f1523764d4adb9258feb5e40
hk1280 <hk1280@nyu.edu>
2025-09-29 22:22:53 -0400

    class 4 notes

--- week4a/sketch.js
+++ week4a/sketch.js
@@ -10,39 +10,39 @@ function setup() {
   // i-- : subtracts 1 from i
   // i-=5 : subtracts 5 from i
 
-  for(let i = 0; i<10; i++){
+  for(let i = 0; i < 10; i++){
     console.log(i);
   }
   
 }
 
 function draw() {
-  background("rgba(250, 199, 110, 1)");
-  noLoop(); // prevents draw from looping
+  background("#eec869ff");
+  // noLoop(); // prevents draw from looping
 
-  for(y = 50; y<height-50; y+=100){
-    for(let x = 50;x<width-50;x+=100){
-      let circleSize;
-      circleSize = map(y,50,height-50,100,10);
-      circle(x,y,circleSize);
+  for(y = 50; y < height - 50; y += 100) {
+    for(let x = 50; x < width - 50; x += 100) {
+      push();
+      translate(x,y);
+      let rotation;
+      rotation = map(y,50,height-50,0,PI);
+      rotate(rotation);
+      // everything within this push/pop block
+      // will be centered, with 0,0 as the center
+      // point
 
-
-  //    push();
-  //    translate(x,50);
-  // // everything within this push/pop block
-  // // will be centered, with 0,0 as the center
-  // // point
-
-  //  strokeWeight(3);
-  //  fill("rgba(250, 239, 144, 1)");
-  //  circle(0,0,100);
-  //  circle(-15,-10,10);
-  //  circle(15,-10,10);
-  // let happiness;
-  // happiness=map(x,0,width,-25,25);
-  // noFill();
-  // arc(0,0,60,60,radians(0-happiness),radians(180+happiness));
-  // pop();
+   strokeWeight(3);
+   fill("rgba(250, 239, 144, 1)");
+   circle(0,0,100);
+   circle(-15,-10,10);
+   circle(15,-10,10);
+   // happiness variable will control the endpoints of
+   // the arc in my smiley face
+   let happiness;
+   happiness=map(x,0,width,-25,25);
+   noFill();
+   arc(0,0,60,60,radians(0-happiness),radians(180+happiness));
+   pop();
   }
 }
   }
