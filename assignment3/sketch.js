6c3bc4dcd5419968ca9245bf6f94b3db84b94f1d
hk1280 <hk1280@nyu.edu>
2025-10-08 12:03:00 -0400

    changesssss

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -3,7 +3,7 @@
 // trying to apply what i have learned so far
 
 let buttonWidth = 200;
-let buttonHeight = 40;
+let buttonHeight = 20;
 let radius = 10;
 
 function setup() {
@@ -14,11 +14,6 @@ function setup() {
 function draw() {
   background("rgba(77, 75, 117, 1)");
   
-
-// table
-fill("rgba(144, 144, 144, 1)");
-rect(0,windowHeight-80,width,height);
-
 // top part of jar
 strokeWeight(4);
 let buttonLeft = width/2-buttonWidth/2; // x coord of top left
@@ -29,6 +24,13 @@ let buttonBottom = buttonTop + buttonHeight; // y coord of bottom right
 fill("rgba(77, 75, 117, 1)");
 stroke("rgb(0,0,0)");
 rect(buttonLeft, buttonTop, buttonWidth, buttonHeight,20) // 20 gives the rounded the edges
+
+
+// table
+fill("rgba(144, 144, 144, 1)");
+noStroke();
+rect(0,windowHeight-80,width,height);
+
 
 // body of the jar
 
@@ -46,9 +48,25 @@ curveVertex(buttonRight-10, buttonBottom);
 curveVertex(buttonRight-10, buttonBottom);
 endShape();
 
+
+
+// SCOBY (symbiotic culture of bacteria & yeast)
+// gelatinous, cellulose-based mat = pellicle
+
+fill("rgba(216, 197, 111, 1)");
+noStroke();
+beginShape();
+curveVertex(buttonLeft, buttonBottom+70);
+curveVertex(buttonLeft-50, buttonBottom+70);
+curveVertex(buttonLeft-50, buttonBottom+100);
+curveVertex(buttonRight+50, buttonBottom+100);
+curveVertex(buttonRight+50, buttonBottom+70);
+curveVertex(buttonRight, buttonBottom+70);
+endShape();
+
 // liquid in the jar
 
-fill("rgba(247, 160, 84, 1)");
+fill("rgba(196, 118, 50, 1)");
 beginShape();
 noStroke();
 curveVertex(buttonLeft, buttonBottom+100);
@@ -60,6 +78,7 @@ curveVertex(buttonRight, buttonBottom+22);
 endShape();
 
 
+
 // sun / moon
 
 fill("rgba(255, 250, 92, 1)");
@@ -69,11 +88,11 @@ circle(windowWidth-150,100,100);
 
 // bubbles in the liquid row by row
 // two dimensional for loop for x and y axis
-// within the contents of the jar
+// within the liquid of the jar
 
 
 for (let x=buttonLeft-20; x<buttonRight+40; x+=30) { 
-for (let y=buttonBottom+80; y<windowHeight-80; y+=60){
+for (let y=buttonBottom+110; y<windowHeight-80; y+=60){
  
     push();
     translate(x,y);
@@ -81,14 +100,37 @@ for (let y=buttonBottom+80; y<windowHeight-80; y+=60){
     strokeWeight(3);
     stroke("rgba(244, 218, 69, 1)");
 
-
     // changing the sizes of the bubbles randomly across the x
     // noLoop so that it's not constantly running and freaking out
 
-    let scalingX = map(x,buttonLeft,buttonRight,random(0.5),random(1.2));
+    let scalingX = map(x,buttonLeft,buttonRight,random(),random(1.2));
     scale(scalingX);
     // let scalingY = map(y,buttonBottom+80,windowHeight-80, random(0.5), random(1.2));
     // scale(scalingY);
+    noLoop();
+
+    circle(0, 0, 20); 
+    pop()
+
+}
+}
+
+// easier for me to think of as a separate nested loop
+// the pellicle area has smaller bubbles in orange
+
+for (let x=buttonLeft-30; x<buttonRight+56; x+=50) { 
+for (let y=buttonBottom+80; y<buttonBottom+110; y+=15){
+ 
+    push();
+    translate(x,y);
+    noFill();
+    strokeWeight(4);
+    stroke("rgba(242, 153, 45, 1)");
+    // changing the sizes of the bubbles randomly across the x
+    // noLoop so that it's not constantly running and freaking out
+
+    let scalingY = map(x,buttonLeft,buttonRight,random(0.5),random(0.5));
+    scale(scalingY);
     noLoop();
 
     circle(0, 0, 20); 
