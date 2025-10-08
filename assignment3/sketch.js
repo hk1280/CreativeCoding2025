0b3078327306fa91e1ca362a49cd1cc323482c4a
hk1280 <hk1280@nyu.edu>
2025-10-08 00:38:23 -0400

    jar

--- assignment3/sketch.js
+++ assignment3/sketch.js
@@ -1,4 +1,5 @@
-
+let buttonWidth = 150;
+let buttonHeight = 20;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,5 +7,31 @@ function setup() {
 }
 
 function draw() {
-  
+  background("rgba(234, 217, 120, 1)");
+  strokeWeight(4);
+
+
+
+let buttonLeft = width/2-buttonWidth/2; // x coord of top left
+let buttonTop = height/3; // y coordinate of top right
+let buttonRight = buttonLeft+buttonWidth; // x coord of bottom right
+let buttonBottom = buttonTop + buttonHeight; // y coord of bottom right
+
+
+rect(buttonLeft, buttonTop, buttonWidth, buttonHeight,20)
+
+beginShape();
+curveVertex(buttonLeft, buttonBottom);
+curveVertex(buttonLeft+10, buttonBottom);
+curveVertex(buttonLeft+5, buttonBottom+22);
+curveVertex(buttonLeft-50, buttonBottom+80);
+curveVertex(buttonLeft-30, windowHeight-80);
+curveVertex(buttonRight+30, windowHeight-80);
+curveVertex(buttonRight+50, buttonBottom+80);
+curveVertex(buttonRight+5, buttonBottom+22);
+curveVertex(buttonRight-10, buttonBottom);
+curveVertex(buttonRight, buttonBottom);
+endShape();
+
+
 }
