d87fd3059c0dee859e35e435ef880d014b14a040
hk1280 <hk1280@nyu.edu>
2025-10-15 12:37:26 -0400

    sushi

--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -1,10 +1,35 @@
 
+// sushi
+// rice - same size
+// different types of fish on top
+// different toppings on fish
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  background("rgb(0,0,0)");
+
   
 }
 
 function draw() {
-  
+  drawRice();
+  drawFish();
+
+}
+
+function drawRice() {
+  push();
+  translate(100,100);
+  fill("rgba(255, 255, 255, 1)");
+  ellipse(0, 18, 160, 60);
+  pop();
+}
+
+function drawFish() {
+  push();
+  translate(100,100);
+  fill("rgba(244, 158, 72, 1)");
+  ellipse(0, 0, 160, 60);
+  pop();
+
 }
