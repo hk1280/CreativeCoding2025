126ba7867db3736d5acff32dae3aa5067c448c39
hk1280 <hk1280@nyu.edu>
2025-10-27 22:51:02 -0400

    week7

--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -1,45 +1,14 @@
 
-// sushi
-// rice - same size
-// different types of fish on top
-// different toppings on fish
-
-let mySushi = [];
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  background("rgb(0,0,0)");
+createCanvas(windowWidth,windowHeight);
+background("rgba(74, 177, 255, 1)");
 
-  
 }
 
 function draw() {
-  drawRice();
-  drawFish();
 
-}
+  translate(width/2, height/2);
+ 
 
-function drawRice() {
-  push();
-  translate(100,100);
-  fill("rgba(255, 255, 255, 1)");
-  ellipse(0, 18, 160, 60);
-  pop();
-}
-
-function drawFish() {
-  push();
-  translate(100,100);
-  fill("rgba(244, 158, 72, 1)");
-  ellipse(0, 0, 160, 60);
-  pop();
-
-}
-
-class Sushi {
-  constructor(x,y,range) {
-  this.x = x;
-  this.y = y;
-  this.range = range;
-  }
 }
