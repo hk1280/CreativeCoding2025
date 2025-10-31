7b95cb92934165e2badb71b12e8b9067a775b4fb
hk1280 <hk1280@nyu.edu>
2025-10-31 19:25:50 -0400

    fixes

--- assignment4/sketch.js
+++ assignment4/sketch.js
@@ -24,14 +24,21 @@ function keyPressed(){
       fish = 'salmon';
     } else if (key == 't'){
       fish = 'tuna';
+    } else if (key == 'e'){
+      fish = 'egg';
     }
 
-    if (key == 's' || key == 't'){
+    // press these keys that are connected to each fish
+    // it will generate different sized sushis
+
+    if (key == 's' || key == 't' || key == 'e'){
       let sizes = ["small", "medium", "large"];
       let randomSize = random(sizes);
 
     let mySushi = new Sushi(randomSize, fish);
     sushi.push(mySushi);
+    // pushes the newly generated sushi into the
+    // the sushi array
 
     }
   }
@@ -41,8 +48,11 @@ class Sushi {
   constructor(size, fish) {
     this.sushiX = random(width);
     this.sushiY = random(height);
-    this.fishHeight = 0.34;
-    this.fishWidth = 1.2;
+    // setting percentages to multiply to width for
+    // the diff ratios
+    this.fishHeight = 0.34; // fish height percentage
+    this.fishWidth = 1.2; // fish width percentage 
+    this.rice = 0.24; // multiply to width to get height of rice
     this.offset = 0.13; 
     // affects the y pos b/c multiply to the x pos
     // to see where the fish sits on the rice
@@ -50,13 +60,10 @@ class Sushi {
     // pick width by size
     if (size == "small") {
       this.width = 110; // width of rice
-      this.rice = 0.24; // multiply to the width to give height of rice
     } else if (size == "medium") {
       this.width = 140;
-      this.rice = 0.26;
     } else if (size == "large") {
-      this.width = 180; 
-      this.rice = 0.28;
+      this.width = 180;
     }
     this.fish = fish;
 
@@ -71,16 +78,24 @@ class Sushi {
       fill("rgba(255, 255, 255, 1)");
       rect(0,0,this.width,this.width*this.rice,10);
 
+      //fish/egg
       if (this.fish == "salmon") {
       fill("rgb(255, 172, 28)");
       } else if (this.fish == "tuna") {  
       fill("rgb(207, 52, 67)");
+      } else if (this.fish == "egg") {
+      fill("rgba(255, 255, 120, 1)");
+      }
+    
+      rect (0, -this.width*this.offset, this.width*this.fishWidth, this.width*this.fishHeight, 30);
+     
+      // nori for the tamago
+      if (this.fish == "egg") {
+        fill("rgb(0,0,0)");
+        rect(0,-this.width*this.offset, this.width*this.fishWidth*0.25, this.width*this.fishHeight);
+      }
 
-}
-
-rect (0, -this.width*this.offset, this.width*this.fishWidth, this.width*this.fishHeight, 30);
-pop();
-
+ pop();
 
       }
     }
