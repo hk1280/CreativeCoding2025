a60e7c785445b0560a0230d741124c8c17cc3a2d
hk1280 <hk1280@nyu.edu>
2025-12-09 14:34:27 -0500

    changes

--- finalproject - craig/sketch.js
+++ finalproject - craig/sketch.js
@@ -1,7 +1,7 @@
 let circleOsc = 0;
 let pMillis = 0;
 let interval = 4000;
-let circleGrowRange = 50;
+let circleGrowRange = 150;
 let circleBaseSize = 250;
 
 function setup() {
@@ -17,11 +17,20 @@ function draw() {
   }
 
   text(timer.toFixed(0),50,50);
+
+  // converts the millis into percentage of cycle
+  // 0 beginning, 1 is end
   let timePercentage = timer/interval;
   text(timePercentage.toFixed(5),50,60);
+  
+  // map to convert the percentage into an angle in radians
+  // when 0 angle is 0, 1 is 2 pi
+  // so over 4 seconds, one full circle around the sin wave
+
   let timeAngle = map(timePercentage,0,1,0,2*PI);
   text(timeAngle.toFixed(5),50,70);
 
+// sin(timeAngle) b/w -1 and 1 as timeAngle goes from 0 to 2 pi
   circleOsc = (sin(timeAngle)*circleGrowRange);
   circle(width/2,height/2,circleOsc+circleBaseSize);
 }
--- finalproject/sketch.js
+++ finalproject/sketch.js
@@ -11,8 +11,17 @@ let video;
 let faces = [];
 let options = { maxFaces: 1, refineLandmarks: false, flipHorizontal: false };
 
+let inhaleDuration = 4000;
+let holdDuration = 4000;
+let exhaleDuration = 4000;
+let totalDuration;
+
+let pMillis = 0;
+
 let circleRadius = 80;
-let noisePosition = 0;
+let circleMin = 80;
+let circleMax = 180;
+
 
 function preload() {
   // Load the faceMesh model
@@ -21,6 +30,9 @@ function preload() {
 
 function setup() {
   createCanvas(640, 480);
+
+  totalDuration = inhaleDuration + holdDuration + exhaleDuration;
+
   // Create the webcam video and hide it
   video = createCapture(VIDEO);
   video.size(640, 480);
@@ -32,6 +44,42 @@ function setup() {
 function draw() {
   // Draw the webcam video
   image(video, 0, 0, width, height);
+
+  let elapsed = millis()-pMillis;
+
+  if(elapsed>totalDuration){
+    pMillis = millis(); //reset timer after interval
+    elapsed = 0; // reset elapsed
+  }
+
+  let guidePhase;
+  let phaseTime;
+
+  if (elapsed < inhaleDuration) {
+    guidePhase = 'INHALE';
+    phaseTime = elapsed / inhaleDuration;
+  } else if (elapsed < inhaleDuration + holdDuration) {
+    guidePhase = 'HOLD';
+    phaseTime = (elapsed - inhaleDuration) / holdDuration;
+  } else {
+    guidePhase = 'EXHALE';
+    phaseTime = (elapsed - inhaleDuration - holdDuration) / exhaleDuration;
+  }
+
+  // setting the radius by phase
+
+  let targetRadius;
+
+  if (guidePhase == 'INHALE') {
+    targetRadius = lerp(circleMin, circleMax, phaseTime);
+  } else if (guidePhase == 'HOLD') {
+    targetRadius = circleMax;
+  } else if (guidePhase == 'EXHALE') {
+    targetRadius = lerp(circleMax, circleMin, phaseTime);
+  }
+
+  circleRadius = lerp(circleRadius, targetRadius, 0.1);
+
   
   if (faces.length>0){
     let upperLip = faces[0].keypoints[13];
@@ -65,36 +113,75 @@ function draw() {
     let mouthRatio = verticalDist / horizontalDist;
 
     // text box in corner stating mouth ratio
-    fill("rgba(146, 170, 221, 1)");
-    rect(0, height-30, 220, 30);
-    fill("rgba(255, 255, 255, 1)");
-    textSize(14);
-    textAlign(LEFT, CENTER);
+    // fill("rgba(146, 170, 221, 1)");
+    // rect(0, height-30, 220, 30);
+    // fill("rgba(255, 255, 255, 1)");
+    // textSize(14);
+    // textAlign(LEFT, CENTER);
 
     // toFixed is a javascript method that is part of the
     // Number.prototype object
     // gives you 3 decimal points below
-    text("mouthRatio: " + mouthRatio.toFixed(3), 10, height-15);
+    // text("mouthRatio: " + mouthRatio.toFixed(3), 10, height-15);
 
-    let phase;
+    let detectedPhase;
+
     if (mouthRatio < 0.08) {
-      phase = 'INHALE';
+      detectedPhase = 'INHALE';
     } else if (mouthRatio > 0.08) {
-      phase = 'EXHALE';
+      detectedPhase = 'EXHALE';
     }
+
+    let inSync = false;
+
+    
 
     textSize(32);
     fill("rgba(255, 255, 255, 1)");
 
-    if (phase == 'INHALE') {
-      text("You are inhaling", 10, 50);
-    } else if (phase == 'EXHALE') {
-      text("You are exhaling", 10, 50);
-    }
-     drawCircle(phase);
+    if (guidePhase == 'INHALE' || guidePhase == 'HOLD') {
+      if (detectedPhase == 'INHALE') inSync = true;
+  } else if (guidePhase == 'EXHALE') {
+    if (detectedPhase == 'EXHALE') inSync = true;
+  }
+
+  if (faces.length == 0) {
+    stroke("rgb(0,0,0)");
+    fill("rgba(214, 162, 246, 1)");
+  } else if (inSync) {
+    stroke("rgb(0,0,0)");
+    fill("rgba(161, 252, 185, 1)");
+  } else {
+    stroke("rgb(0,0,0)");
+    fill("rgba(157, 201, 255, 1)");
   }
  
+  strokeWeight(3);
+  circle(width/2, height/2, circleRadius*2);
 
+  textSize(28);
+  let instructionText = '';
+  if (guidePhase == 'INHALE') {
+    instructionText = "Breathe in slowly";
+  } else if (guidePhase == 'HOLD') {
+    instructionText = "Hold your breath";
+  } else {
+    instructionText = "Breathe out slowly"
+  }
+
+  text(instructionText, width/2, height/2);
+
+  textAlign(LEFT, TOP);
+  textSize(16);
+  text("Guide phase: " + guidePhase, 10, 10);
+
+  if (faces.length >0) {
+    text("Detected phase: " + detectedPhase, 10, height-40);
+    text("mouthRatio: " + mouthRatio.toFixed(3), 10, height-20);
+  } else {
+    text("No face detected",10,height-30);
+
+  }
   // // Draw all the tracked face points with the number
   // for (let i = 0; i < faces.length; i++) {
   //   let face = faces[i];
@@ -106,53 +193,54 @@ function draw() {
   //   }
   // }
 }
-
-function drawCircle(phase) {
-
-  // setting the size of the circle based on phase
-  let targetRadius;
-  if (phase == 'INHALE') {
-    targetRadius = 60;
-  } else if (phase == 'EXHALE') {
-    targetRadius = 120;
-  }
-
-  // smooth transition b/w sizes
-  // lerp(start,stop,amt)
-  circleRadius = lerp(circleRadius, targetRadius, 0.05);
-
-  // setting the speed based on phase
-  let noiseSpeed;
-  if (phase == 'INHALE') {
-    noiseSpeed = 0.002;
-  } else if (phase == 'EXHALE'){
-    noiseSpeed = 0.006;
-  }
-  
-  noisePosition = noisePosition + noiseSpeed;
-
-  beginShape();
-
-  // drawing the circle
-  // each loop iteration places one point on the circle
-  // smaller the incrementation, the smoother it is
-
-  for (let i=0; i<TWO_PI; i+=0.1) {
-
-    // noise(x, y, z)
-    // returns a value b/w 0 and 1
-    let wiggle = noise(cos(i), sin(i), noisePosition) * 20;
-    let radiusWiggle = circleRadius + wiggle;
-
-    let x = width/2 + cos(i) * radiusWiggle;
-    let y = height/2 + sin(i) * radiusWiggle;
-
-    vertex(x,y);
-
-  }
-  endShape(CLOSE);
-
 }
+
+// function drawCircle(phase) {
+
+//   // setting the size of the circle based on phase
+//   let targetRadius;
+//   if (phase == 'INHALE') {
+//     targetRadius = 60;
+//   } else if (phase == 'EXHALE') {
+//     targetRadius = 120;
+//   }
+
+//   // smooth transition b/w sizes
+//   // lerp(start,stop,amt)
+//   circleRadius = lerp(circleRadius, targetRadius, 0.05);
+
+//   // setting the speed based on phase
+//   let noiseSpeed;
+//   if (phase == 'INHALE') {
+//     noiseSpeed = 0.002;
+//   } else if (phase == 'EXHALE'){
+//     noiseSpeed = 0.006;
+//   }
+  
+//   noisePosition = noisePosition + noiseSpeed;
+
+//   beginShape();
+
+//   // drawing the circle
+//   // each loop iteration places one point on the circle
+//   // smaller the incrementation, the smoother it is
+
+//   for (let i=0; i<TWO_PI; i+=0.1) {
+
+//     // noise(x, y, z)
+//     // returns a value b/w 0 and 1
+//     let wiggle = noise(cos(i), sin(i), noisePosition) * 20;
+//     let radiusWiggle = circleRadius + wiggle;
+
+//     let x = width/2 + cos(i) * radiusWiggle;
+//     let y = height/2 + sin(i) * radiusWiggle;
+
+//     vertex(x,y);
+
+//   }
+//   endShape(CLOSE);
+
+// }
 
 
 // Callback function for when faceMesh outputs data
@@ -161,7 +249,7 @@ function gotFaces(results) {
   faces = results;
 }
 
-function mousePressed(){
+function mousePressed() {
   if (faces.length>0) {
   let mouth = faces[0].lips.keypoints;
   console.log(mouth);
